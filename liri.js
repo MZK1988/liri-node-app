07c06f6c183690a4585e7469f38618c7ce7c2173
MZK1988 <max@korenvaes.com>
2018-11-01 18:22:06 -0500

    first totally complete homework

--- liri.js
+++ liri.js
@@ -173,11 +173,12 @@ else if (command === "spotify-this-song" && search != "") {
       var dataArr = data.split(",");
 
       // We will then re-display the content as an array for later use.
-      
+      var command = dataArr[0];
+      console.log(command);
       var execute = dataArr[1];
       var concatenate = execute.split(" ").join("+");
       console.log(concatenate);
-
+      if (command === "spotify-this-song") {
       spotify.search({ type: 'track', query: concatenate }, function (err, data) {
         if (err) {
           console.log('Error occurred: ' + err);
@@ -185,9 +186,6 @@ else if (command === "spotify-this-song" && search != "") {
         } else {
     
           for (let i = 0; i < data.tracks.items.length; i++) {
-    
-            
-    
               console.log("______________________________________")
               console.log("ARTIST: ")
               console.log(data.tracks.items[i].artists[0].name);
@@ -198,24 +196,76 @@ else if (command === "spotify-this-song" && search != "") {
               console.log("ALBUM: ")
               console.log(data.tracks.items[i].album.name);
               console.log("______________________________________")
-    
-    
-    
-    
             
           }
         }
     
       });
+      } else if(command === "movie-this") {
 
+          console.log("hello world")
 
+          var queryUrl = "http://www.omdbapi.com/?t=" + concatenate + "&y=&plot=short&apikey=trilogy";
 
+          request(queryUrl, function (error, response, body) {
 
+            // If the request is successful (i.e. if the response status code is 200)
+            if (!error && response.statusCode === 200) {
+        
+              // Parse the body of the site and recover just the imdbRating
+              // (Note: The syntax below for parsing isn't obvious. Just spend a few moments dissecting it).
+              // console.log(search + "'s rating is: " + JSON.parse(body));
+        
+              console.log("Title:");
+              console.log(JSON.parse(body).Title);
+              console.log("-------------------");
+              console.log("Release Year:");
+              console.log(JSON.parse(body).Year);
+              console.log("-------------------");
+              console.log("IMDB Rating:");
+              console.log(JSON.parse(body).imdbRating);
+              console.log("-------------------");
+              console.log("Release Country:");
+              // console.log(JSON.parse(body).ratings.source);
+              console.log(JSON.parse(body).Country);
+              console.log("-------------------");
+              console.log("Release Language:");
+              console.log(JSON.parse(body).Language);
+              console.log("-------------------");
+              console.log("Plot:");
+              console.log(JSON.parse(body).Plot);
+              console.log("-------------------");
+              console.log("Actors:");
+              console.log(JSON.parse(body).Actors);
+              console.log("-------------------");
+            }
+          });
 
+      } else if (command === "concert-this") {
+        console.log("hello world")
+        var queryUrl = "https://rest.bandsintown.com/artists/" + concatenate + "/events?app_id=codingbootcamp"
 
+        request(queryUrl, function (error, response, body) {
 
+          // If the request is successful (i.e. if the response status code is 200)
+          if (!error && response.statusCode === 200) {
+            console.log("ARTIST:")
+            console.log(name);
+            console.log("-------------------");
+            console.log("VENUE NAME:");
+            console.log(JSON.parse(body)[0].venue.name);
+            console.log("-------------------");
+            console.log("VENUE CITY:");
+            console.log(JSON.parse(body)[0].venue.city);
+            console.log("-------------------");
+            console.log("DATE/TIME OF PERFORMANCE:");
+            console.log(JSON.parse(body)[0].datetime);
+            console.log("-------------------");
+      
+          }
+        }
 
-
+        )}
     });
 
   }
